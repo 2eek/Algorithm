221b462dc61d8a850a3bfc35f5de6f8f8fdd9ec0
2eek <123248546+2eek@users.noreply.github.com>
2024-04-16 22:35:12 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18536 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,16 +1,13 @@
- import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        
-    
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        
-        sc.close();
-        
-        for(int i = 1; i<10; i++){
-            System.out.println(a+ " * "+i+" = "+(a*i));
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner 객체 생성
+        int N = sc.nextInt(); // 사용자로부터 정수 입력 받음
+        int i; // 반복문을 위한 변수 선언
+
+        for (i = 1; i <= 9; i++) { // 1부터 8까지 반복
+            System.out.println( N +" * "+ i +" = "+ N * i); // N에 i를 곱한 결과 출력
         }
     }
-    
 }
